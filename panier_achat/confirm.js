ff36f08511fb2e66d4efc456a5d6a7e7573ab817
Jiyun Grosjean <115343314+bergam99@users.noreply.github.com>
2022-11-28 12:52:31 +0100

    Delete panier_achat directory

--- panier_achat/confirm.js
+++
@@ -1,9 +0,0 @@
-// Menu Burger
-
-const burger = document.querySelector(".menuBurger");
-const navHeader = document.querySelector(".navLink");
-
-burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    navHeader.classList.toggle("active");
-})
